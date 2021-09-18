04f3ad44152e6e9de1bc7be430b5e9f9419f9ea7
tomas <tomas.o.sc@gmail.com>
2021-09-18 08:08:46 -0700

    Update NB_chicago.mat, NB_estimator.m, and 5 more files...

--- paper_simulations/Single_simulation_Chicago/NB_estimator.m
+++ paper_simulations/Single_simulation_Chicago/NB_estimator.m
@@ -8,8 +8,8 @@ for i = 1:size(mesh, 1)
     prod1 = probY1;
     prod0 = probY0;
     for k = 1:M
-        prod1 = prod1 * points_probability_eps([mesh(i,:),1;data(k,:)], lambda, Lmax, eps);
-        prod0 = prod0 * points_probability_eps([mesh(i,:),0;data(k,:)], lambda, Lmax, eps);
+        prod1 = prod1 * points_probability_eps([mesh(i,:),1;data(k,:)], lambda, Lmax, eps) / probY1;
+        prod0 = prod0 * points_probability_eps([mesh(i,:),0;data(k,:)], lambda, Lmax, eps) / probY0;
     end    
 	estimation(i) = double(prod1 > prod0);
 end
